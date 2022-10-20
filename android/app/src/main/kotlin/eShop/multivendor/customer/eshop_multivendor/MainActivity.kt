fdfdabf5d94ff9dd66262eed8ae2168c83062d61
HamzaDawood21 <90140966+HamzaDawood21@users.noreply.github.com>
2022-10-21 02:14:14 +0500

    Add files via upload

---
+++ android/app/src/main/kotlin/eShop/multivendor/customer/eshop_multivendor/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.iconic.computer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
+
